b6b5f8bbfe6634dbde0420dd42eb18b821d8c20f
rbtnn <naru123456789@gmail.com>
2018-11-09 23:22:12 +0900

    update appveyor

--- src/appveyor.bat
+++ src/appveyor.bat
@@ -4,7 +4,7 @@
 setlocal ENABLEDELAYEDEXPANSION
 cd %APPVEYOR_BUILD_FOLDER%
 
-cd src
+rem cd src
 rem echo "Building MinGW 32bit console version"
 rem set PATH=c:\msys64\mingw32\bin;%PATH%
 rem mingw32-make.exe -f Make_ming.mak GUI=no OPTIMIZE=speed IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
@@ -26,22 +26,36 @@ rem echo "Building MSVC 64bit console Version"
 sed -e "s/\$(LINKARGS2)/\$(LINKARGS2) | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
 rem nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=no IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
 rem nmake -f Make_mvc2.mak clean
-
-:: build MSVC huge version with python and channel support
-:: GUI needs to be last, so that testing works
-echo "Building MSVC 64bit GUI Version"
-if "%FEATURE%" == "HUGE" (
-    nmake -f Make_mvc2.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
-) ELSE (
-    nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
-)
-.\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_msvc.txt
-
-:: Restore vim.exe, tests will run with this.
+rem 
+rem :: build MSVC huge version with python and channel support
+rem :: GUI needs to be last, so that testing works
+rem echo "Building MSVC 64bit GUI Version"
+rem if "%FEATURE%" == "HUGE" (
+rem     nmake -f Make_mvc2.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
+rem ) ELSE (
+rem     nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+rem )
+rem .\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_msvc.txt
+rem 
+rem :: Restore vim.exe, tests will run with this.
 rem move /Y testdir\vim.exe .
 rem echo "version output MinGW"
 rem type ver_ming.txt
-echo "version output MVC"
-type ver_msvc.txt
-cd ..
+rem echo "version output MVC"
+rem type ver_msvc.txt
+rem cd ..
 
+cd src
+    sed -e "s/\$(LINKARGS2)/\$(LINKARGS2) | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
+
+    nmake -f Make_mvc2.mak DIRECTX=yes CPU=i386 CHANNEL=yes OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35 FEATURES=%FEATURE% || exit 1
+    .\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_msvc-i386.txt
+    type ver_msvc-i386.txt
+    move /Y .\gvim.exe .\gvim-i386.exe
+
+    nmake -f Make_mvc2.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
+    .\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_msvc-AMD64.txt
+    type ver_msvc-AMD64.txt
+    move /Y .\gvim.exe .\gvim-AMD64.exe
+
+cd ..
